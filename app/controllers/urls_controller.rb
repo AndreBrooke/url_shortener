fce845457a9b9bb260d7ad9e0401349d0d41140e
Andre Brooke <andrebrooke1@gmail.com>
2017-09-13 18:12:14 +0800

    Added url submission form

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,8 +3,27 @@ class UrlsController < ApplicationController
 		@urls = Url.all
 	end
 
+	def new
+		@url = Url.new
+	end
+
+	def create
+		@url = Url.new(url_params)
+		@url.short
+		
+		if @url.save
+			redirect_to @url
+		else
+			render 'new'
+		end
+	end
+
 	def show
 	
 		@url = Url.find(params[:id])
+	end
+
+	def url_params
+		params.require(:url).permit(:long_url)
 	end
 end
